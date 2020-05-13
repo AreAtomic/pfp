4a5484e239df13ed838b7afbf67ed6b2d4d1dc4c
Areatomic <aureliensebe@gmail.com>
2020-05-13 08:26:04 +0200

    Build

---
+++ build/precache-manifest.812679c3894241523a6de0f005e5505d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "553e8712389ee9119d3498afe7492cd1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65e77cd1fc2ba13728b0",
+    "url": "/static/css/main.c7446496.chunk.css"
+  },
+  {
+    "revision": "726fd11fe447309f9666",
+    "url": "/static/js/2.4c791976.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4c791976.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65e77cd1fc2ba13728b0",
+    "url": "/static/js/main.ee580e49.chunk.js"
+  },
+  {
+    "revision": "ddd8666b2b430a4cc6e1",
+    "url": "/static/js/runtime-main.c90964d3.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
